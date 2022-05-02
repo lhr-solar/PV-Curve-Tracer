b6c9d0a7605d2754e30ace6efef7f3b2899ce7b6
Matthew Yu <Matthewjkyu@gmail.com>
2022-05-02 12:24:46 -0500

    curve tracer test

--- main.cpp
+++ main.cpp
@@ -18,140 +18,145 @@ const bool __DEBUG_TUNING__ = false;
 
 // 19200 baud rate.
 #define BLINKING_RATE           250ms
-#define SETTLING_TIME           3ms
-#define NUM_CONTROLLER_STATES   3
+#define SETTLING_TIME           15000 // us
+#define ITERATIONS              5
 
-DigitalOut ledHeartbeat(D3);
-AnalogIn sensorVoltage(A0);
-AnalogIn sensorCurrent(A6);
+DigitalOut ledHeartbeat(D1);
+AnalogIn sensorVoltage(A6);
+AnalogIn sensorCurrent(A0);
 AnalogOut dacControl(A3);
 CAN can(D10, D2);
+CANMessage msg;
 
 /** Tickers. */
 LowPowerTicker tickHeartbeat;
+void heartbeat(void) {ledHeartbeat = !ledHeartbeat;}
 
-void heartbeat(void) { ledHeartbeat = !ledHeartbeat;}
-
-/** 
- * A section struct contains four parameters:
- * - The [start, stop) percentage of the gate voltage
- * - The percent resolution of the gate voltage step.
- * - The number of times each step should be repeated.
- */
-struct section {
-    float startPercent;
-    float stopPercent;
-    float resolutionPercent;
-    uint8_t repetition;
+enum Mode {
+    CELL,
+    MODULE,
+    ARRAY
 };
 
-struct section controller[NUM_CONTROLLER_STATES] {
-    {   // MOSFET OFF RANGE [x, 0.35)
-        .startPercent=0.0, 
-        .stopPercent=0.35,
-        .resolutionPercent=0.02,
-        .repetition=10
-    },
-    {   // MOSFET TRANSITION RANGE [0.35, 0.40)
-        .startPercent=0.35,
-        .stopPercent=0.40,
-        .resolutionPercent=0.00025, // <0.00025 will be too small to register changes on DAC. TODO: reduce gate OP AMP gain.
-        .repetition=30
-    },
-    {   // MOSFET ON RANGE [0.40, 0.6]
-        .startPercent=0.40,
-        .stopPercent=0.6,
-        .resolutionPercent=0.02,
-        .repetition=10
+float calibrateDACOut(float in) {
+    // const float slope = 9.9539;
+    // const float intercept = 0.0583;
+    return in;// * slope + intercept;
+}
+
+float calibrateVoltageSensor(float in, float current, int numIterations, enum Mode mode) {
+    // + calibration offset for internal PCB resistances causing a voltage drop prior to the voltage sensor. 
+    switch (mode) {
+        case CELL:
+            return 1.1047 * in / numIterations;
+        case MODULE:
+            return 5.4591 * in / numIterations;
+        case ARRAY:
+            return 111.8247 * in / numIterations;
+        default:
+            tickHeartbeat.detach();
+            ledHeartbeat = 1;
+            while (1) {}
     }
-};
+}
+
+float calibrateCurrentSensor(float in, int numIterations) {
+    return 8.1169 * in / numIterations;
+}
 
 int main() {
     tickHeartbeat.attach(&heartbeat, 500ms);
-    dacControl = 0.0; // Default force low.
+    dacControl = 0.0; // 1.0 for open circuit, 0.0 for short circuit
+    enum Mode mode = MODULE;
 
-    uint8_t controllerState = 0;
     if (__DEBUG_TUNING__) {
-        int add = 0;
-        while (1) {
-            double dacControlAvg = 0;
-            double sensorVoltageAvg = 0;
-            double sensorCurrentAvg = 0;
+        printf("DEBUG MODE\n");
+        ThisThread::sleep_for(5000ms);
 
-            /* Capture output. */    
-            for (uint8_t i = 0; i < 100; ++i) {
-                ThisThread::sleep_for(5ms);
-                dacControlAvg += dacControl.read();
-                sensorVoltageAvg += sensorVoltage.read();
-                sensorCurrentAvg += sensorCurrent.read();
+        while(1) {
+            ThisThread::sleep_for(1000ms);
+            float sVolt = 0.0;
+            float sCurr = 0.0;
+
+            for (uint8_t j = 0; j < ITERATIONS; ++j) {
+                wait_us(SETTLING_TIME);
+                sVolt += sensorVoltage.read();
+                sCurr += sensorCurrent.read();
             }
-            /* Average samples. */
-            dacControlAvg /= 100;
-            sensorVoltageAvg /= 100;
-            sensorCurrentAvg /= 100;
 
+            float dacVolt = calibrateDACOut(dacControl);
+            sCurr = calibrateCurrentSensor(sCurr, ITERATIONS);
+            sVolt = calibrateVoltageSensor(sVolt, sCurr, ITERATIONS, mode);
             printf(
-                "Gate (V): %f\tVoltage (V): %f\tCurrent (A): %f\n", 
-                dacControlAvg*9.9539 + 0.0583, 
-                sensorVoltageAvg*1.1, // CELL CALIBRATION
-                // sensorVoltageAvg*1.1*6.6-0.05, // MODULE CALIBRATION
-                // sensorVoltageAvg*67, // ARRAY CALIBRATION
-                (sensorCurrentAvg+0.1342)/0.1359 - 1
+                // "%f,%f,%f,%f\n", 
+                "Open Circuit\nGate (V): %f, VSense (V): %f, ISense (A): %f, V*I (W): %f\n", 
+                dacVolt, 
+                sVolt,
+                sCurr,
+                sVolt * sCurr
             );
         }
     } else {
-        printf("\n\nGate (V)\tVoltage (V)\tCurrent (A)\n");
-        ThisThread::sleep_for(5000ms);
+        printf("SCAN MODE\n");
+        printf("\n\nGate (V),Voltage (V),Current (A)\n");
+        ThisThread::sleep_for(10000ms);
+
+        bool forward = true;
         while (1) {
-            /* Execute current controller state. */
-            #define START_PERCENT   controller[controllerState].startPercent
-            #define STOP_PERCENT    controller[controllerState].stopPercent
-            #define RES_PERCENT     controller[controllerState].resolutionPercent
-            #define REPETITION      controller[controllerState].repetition
 
-            /* In place update of the DAC to the Gate MOSFET. */
-            for (dacControl = START_PERCENT; 
-                dacControl < STOP_PERCENT;
-                dacControl = dacControl + RES_PERCENT) {
+            // [0.325, 0.4, 0.00025]: 25 iterations at 1 mS each.
+            if (forward) {
+                for (float i = 0.25; i <= 0.5; i += 0.001) {
+                    dacControl = i;
+                    float sVolt = 0.0;
+                    float sCurr = 0.0;
 
-                float dacControlAvg = 0;
-                float sensorVoltageAvg = 0;
-                float sensorCurrentAvg = 0;
-                /* Capture output. */    
-                for (uint8_t i = 0; i < REPETITION; ++i) {
-                    /* Allow for settling time. */
-                    ThisThread::sleep_for(SETTLING_TIME);
-                    dacControlAvg += dacControl.read();
-                    sensorVoltageAvg += sensorVoltage.read();
-                    sensorCurrentAvg += sensorCurrent.read();
+                    for (uint8_t j = 0; j < ITERATIONS; ++j) {
+                        wait_us(SETTLING_TIME);
+                        sVolt += sensorVoltage.read();
+                        sCurr += sensorCurrent.read();
+                    }
+
+                    float dacVolt = calibrateDACOut(dacControl);
+                    sCurr = calibrateCurrentSensor(sCurr, ITERATIONS);
+                    sVolt = calibrateVoltageSensor(sVolt, sCurr, ITERATIONS, mode);
+                    printf(
+                        "%f,%f,%f,%f\n", 
+                        dacVolt, 
+                        sVolt,
+                        sCurr,
+                        sVolt * sCurr
+                    );
                 }
+                forward = false;
+            } else {
+                for (float i = 0.5; i >= 0.25; i -= 0.001) {
+                    dacControl = i;
+                    float sVolt = 0.0;
+                    float sCurr = 0.0;
 
-                dacControlAvg /= REPETITION;
-                sensorVoltageAvg /= REPETITION;
-                sensorCurrentAvg /= REPETITION;
+                    /* Capture the average of ITERATIONS reads. */
+                    for (uint8_t j = 0; j < ITERATIONS; ++j) {
+                        wait_us(SETTLING_TIME);
+                        sVolt += sensorVoltage.read();
+                        sCurr += sensorCurrent.read();
+                    }
 
-                printf(
-                    "%f\t%f\t%f\n", 
-                    dacControlAvg*9.9539 + 0.0583, 
-                    sensorVoltageAvg*1.1, // CELL CALIBRATION
-                    // sensorVoltageAvg*1.1*6.6-0.05, // MODULE CALIBRATION
-                    // sensorVoltageAvg*67, // ARRAY CALIBRATION
-                    (sensorCurrentAvg+0.1342)/0.1359 - 1
-
-                    
-                    // dacControlAvg*3.3/3, 
-                    // (sensorVoltageAvg*3.3/3-.01)*1.02, // CELL CALIBRATION
-                    // sensorCurrentAvg*3.3/0.4*1.03
-                );
+                    float dacVolt = calibrateDACOut(dacControl);
+                    sCurr = calibrateCurrentSensor(sCurr, ITERATIONS);
+                    sVolt = calibrateVoltageSensor(sVolt, sCurr, ITERATIONS, mode);
+                    printf(
+                        "%f,%f,%f,%f\n", 
+                        dacVolt, 
+                        sVolt,
+                        sCurr,
+                        sVolt * sCurr
+                    );
+                }
+                forward = true;
             }
-
-            #undef START_PERCENT
-            #undef STOP_PERCENT
-            #undef RES_PERCENT
-            #undef REPETITION
-
-            /* Shift to the next controller state. */
-            controllerState = (controllerState + 1) % NUM_CONTROLLER_STATES;
         }
     }
 }
+
+
